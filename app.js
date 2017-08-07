f136ecc4605091222004d80029e4745779ae338c
meikongwuzui <865419440@qq.com>
2017-08-07 14:48:42 +0800

    static file

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 var express=require('express');
 var app=express();
 
+var options={
+    dotfiles: 'ignore',
+    etag: false,
+    extensions: ['htm', 'html'],
+    index: false,
+    maxAge: '1d',
+    redirect: false,
+    setHeaders: function (res, path, stat) {
+        res.set('x-timestamp', Date.now());
+    }
+}
+
+app.use(express.static('src',options));
+
 app.post('/',function(req,res){
     res.send("Hello Post");
 });
